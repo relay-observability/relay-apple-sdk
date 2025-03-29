4795c98c0846aa639ac0e4028dc97cfd1e05ace3
Caleb Davis <hello.calebdavis@gmail.com>
2025-03-28 21:07:52 -0500

    Add two unit tests

--- Tests/RelayCoreTests/RelayTests.swift
+++ Tests/RelayCoreTests/RelayTests.swift
@@ -1,4 +1,12 @@
 @testable import RelayCore
 import XCTestCase
 
-final class RelayTests: XCTestCase {}
+final class RelayTests: XCTestCase {
+    func test_validTest() {
+        XCTAssertTrue(true)
+    }
+    
+    func test_invalidTest() {
+        XCTAssertTrue(false)
+    }
+}
