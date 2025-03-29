c97746b01f2f463ec5568df9fdf9e746ab53b17f
Caleb Davis <hello.calebdavis@gmail.com>
2025-03-29 13:45:03 -0500

    update copyright headers

--- Sources/RelayCore/Internal/Persistence/Serializers/JSONEventSerializer.swift
+++ Sources/RelayCore/Internal/Persistence/Serializers/JSONEventSerializer.swift
@@ -2,7 +2,12 @@
 //  JSONEventSerializer.swift
 //  RelayCore
 //
-
+//  Created on March 28, 2025 as part of the Relay open-source observability SDK.
+//  Copyright © 2025 Relay Contributors. All rights reserved.
+//
+//  Licensed under the MIT License.
+//  See LICENSE.md in the project root for license information.
+//
 import Foundation
 
 /// A simple, pluggable serializer that encodes an array of `RelayEvent` into JSON `Data`.
--- Sources/RelayCore/Internal/Persistence/Writers/EventPersisting.swift
+++ Sources/RelayCore/Internal/Persistence/Writers/EventPersisting.swift
@@ -1,3 +1,14 @@
+//
+//  EventPersisting.swift
+//  RelayCore
+//
+//  Created on March 28, 2025 as part of the Relay open-source observability SDK.
+//  Copyright © 2025 Relay Contributors. All rights reserved.
+//
+//  Licensed under the MIT License.
+//  See LICENSE.md in the project root for license information.
+//
+
 import Foundation
 
 /// Protocol for writing Relay events to a persistent store (e.g. disk, memory, etc.).
